6b322042c0ed9df59a80940b0951171a554a5c40
Alastair Gould <alastair.gould@asos.com>
2018-04-14 01:12:56 +0100

    Basic middleware added

---
+++ Correlator/CorrelatorMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace Correlator
+{
+    public class CorrelatorMiddleware : IMiddleware
+    {
+        private readonly string _correlationHeader;
+        private readonly Func<string> _generateCorrelationId;
+
+        public CorrelatorMiddleware(string correlationHeader, Func<string> generateCorrelationId)
+        {
+            _correlationHeader = correlationHeader ?? throw new ArgumentNullException(nameof(correlationHeader));
+            _generateCorrelationId = generateCorrelationId ?? throw new ArgumentNullException(nameof(generateCorrelationId));
+        }
+        
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var correlationId = context.Request.Headers[_correlationHeader];
+
+            if (StringValues.IsNullOrEmpty(correlationId))
+            {
+                correlationId = new StringValues(_generateCorrelationId());
+            }
+
+            context.Response.Headers[_correlationHeader] = correlationId;
+            context.TraceIdentifier = correlationId;
+
+            if (next != null)
+            {
+                await next(context);
+            }
+        }
+    }
+}
